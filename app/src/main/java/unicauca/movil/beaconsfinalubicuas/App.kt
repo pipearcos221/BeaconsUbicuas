d01b8f8523af97fb5c645301f090a03589b2c451
jlbeltran94 <jlbeltran94@gmail.com>
2017-12-09 18:48:54 -0500

    cambia color segun cercania del beacon

--- app/src/main/java/unicauca/movil/beaconsfinalubicuas/App.kt
+++ app/src/main/java/unicauca/movil/beaconsfinalubicuas/App.kt
@@ -2,6 +2,8 @@ package unicauca.movil.beaconsfinalubicuas
 
 import android.app.Application
 import android.content.Intent
+import android.content.SharedPreferences
+import android.util.Log
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion
 import com.estimote.coresdk.recognition.packets.Beacon
 import com.estimote.coresdk.service.BeaconManager
@@ -11,33 +13,64 @@ import unicauca.movil.beaconsfinalubicuas.receivers.BeaconReceiver
  * Created by Asus on 9/12/2017.
  */
 
-class App : Application(), BeaconManager.BeaconRangingListener {
+class App : Application(), BeaconManager.BeaconRangingListener, BeaconManager.BeaconMonitoringListener {
+
 
     override fun onCreate() {
         super.onCreate()
+
         val manager = BeaconManager(this)
         manager.connect {
+            manager.startMonitoring(BeaconRegion("pinturas", null,
+                    null, null))
             manager.startRanging(BeaconRegion("pinturas", null,
                     null, null))
             manager.setRangingListener(this)
+            manager.setMonitoringListener(this)
+
         }
     }
 
     override fun onBeaconsDiscovered(beaconRegion: BeaconRegion?, beacons: MutableList<Beacon>) {
         if(beacons.isNotEmpty()){
-
-            val major = beacons!![0].major
-            val minor = beacons[0].minor
-            val rssi = beacons[0].rssi
+            val major = beacons.first().major
+            val minor = beacons.first().minor
+            val rssi = beacons.first().rssi
 
             val intent =  Intent(BeaconReceiver.ACTION)
             intent.putExtra(BeaconReceiver.EXTRA_MAJOR, major)
             intent.putExtra(BeaconReceiver.EXTRA_MINOR, minor)
             intent.putExtra(BeaconReceiver.EXTRA_RSSI, rssi)
 
+            sendBroadcast(intent)
+        }
+
+    }
+
+    override fun onExitedRegion(beaconRegion: BeaconRegion?) {
+        val intent = Intent(BeaconReceiver.ACTION)
+        intent.putExtra(BeaconReceiver.EXTRA_MAJOR, 0)
+        intent.putExtra(BeaconReceiver.EXTRA_MINOR, 0)
+        intent.putExtra(BeaconReceiver.EXTRA_RSSI, 0)
+
+        sendBroadcast(intent)
+    }
+
+    override fun onEnteredRegion(beaconRegion: BeaconRegion?, beacons: MutableList<Beacon>) {
+        if(beacons.isNotEmpty()) {
+            val major = beacons.first().major
+            val minor = beacons.first().minor
+            val rssi = beacons.first().rssi
+
+            val intent = Intent(BeaconReceiver.ACTION)
+            intent.putExtra(BeaconReceiver.EXTRA_MAJOR, major)
+            intent.putExtra(BeaconReceiver.EXTRA_MINOR, minor)
+            intent.putExtra(BeaconReceiver.EXTRA_RSSI, rssi)
 
             sendBroadcast(intent)
         }
     }
+
+
 
 }
