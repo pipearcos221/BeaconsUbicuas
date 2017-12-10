3d905386d5ce66683db2ee99a12d49e771f01c1d
Felipe Arcos <pipearcos221@gmail.com>
2017-12-10 17:11:49 -0500

    creada navegacion del login al main

--- app/src/main/java/unicauca/movil/beaconsfinalubicuas/LoginActivity.kt
+++ app/src/main/java/unicauca/movil/beaconsfinalubicuas/LoginActivity.kt
@@ -1,12 +1,27 @@
 package unicauca.movil.beaconsfinalubicuas
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jakewharton.rxbinding2.view.clicks
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
+import unicauca.movil.beaconsfinalubicuas.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        btnLogin.clicks()
+                .subscribe{startActivity<MainActivity>()}
     }
 }
