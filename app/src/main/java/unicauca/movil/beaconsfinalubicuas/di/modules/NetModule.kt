a3c8a3c1d53f03dc94ed8af24d02b800741ca546
Felipe Arcos <pipearcos221@gmail.com>
2017-12-11 00:52:21 -0500

    creando funcion para el login

--- app/src/main/java/unicauca/movil/beaconsfinalubicuas/di/modules/NetModule.kt
+++ app/src/main/java/unicauca/movil/beaconsfinalubicuas/di/modules/NetModule.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import unicauca.movil.beaconsfinalubicuas.model.LoginResponse
+import unicauca.movil.beaconsfinalubicuas.model.UserLogin
+import unicauca.movil.beaconsfinalubicuas.net.UserClient
 import javax.inject.Singleton
 
 /**
@@ -26,4 +30,10 @@ class NetModule{
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .baseUrl("https://192.168.0.12:3000/")
             .build()
+
+    @Provides
+    @Singleton
+    fun provideUserClient(retrofit: Retrofit): UserClient =
+            retrofit.create(UserClient::class.java)
+
 }
--- app/src/main/java/unicauca/movil/beaconsfinalubicuas/model/UserLogin.kt
+++ app/src/main/java/unicauca/movil/beaconsfinalubicuas/model/UserLogin.kt
@@ -4,5 +4,6 @@ package unicauca.movil.beaconsfinalubicuas.model
  * Created by Asus on 10/12/2017.
  */
 
+class UserLogin (var username:String,var password:String)
 class LoginResponse(var success:Boolean, var player: Player)
 class RegisterResponse(var success: Boolean, var exist: Boolean)
